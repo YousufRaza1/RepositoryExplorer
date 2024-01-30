453bf61308a25dbbb462d67ddac741571ba833fc
YousufRaza1 <yousuf.ice17@gmail.com>
2024-01-30 21:30:33 +0600

    first commit

---
+++ RepositoryExplorer/RepositoryExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryExplorerApp.swift
+//  RepositoryExplorer
+//
+//  Created by Yousuf on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RepositoryExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
