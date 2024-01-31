2ab608d2f03f392eaf72486f12c83b777b5421d4
YousufRaza1 <yousuf.ice17@gmail.com>
2024-01-31 09:49:49 +0600

    WIP: add navigation

--- RepositoryExplorer/Views/RepositoryListScreen/RepositoryListScreen.swift
+++ RepositoryExplorer/Views/RepositoryListScreen/RepositoryListScreen.swift
@@ -11,30 +11,32 @@ struct RepositoryListScreen: View {
     @StateObject private var viewModel = RepositoryListViewModel()
     @State var pageNumber = 1
     var body: some View {
-        ZStack {
-            ScrollView {
-                VStack {
-                    ForEach(viewModel.repositorys) { repo in
-                        RepoItemView(repo: repo)
-                            .padding(.vertical, 1)
-                    }
-                }
-                .padding(16)
-            }
-            .simultaneousGesture(
-                DragGesture().onChanged {
-                    let isScrollDown = $0.translation.height < 0
-                    if isScrollDown {
-                        Task {
-                            self.pageNumber += 1
-                            await viewModel.getRepos(pageNumber: pageNumber)
+        NavigationView {
+            ZStack {
+                ScrollView {
+                    VStack {
+                        ForEach(viewModel.repositorys) { repo in
+                            RepoItemView(repo: repo)
+                                .padding(.vertical, 1)
                         }
                     }
-                })
+                    .padding(16)
+                }
+                .simultaneousGesture(
+                    DragGesture().onChanged {
+                        let isScrollDown = $0.translation.height < 0
+                        if isScrollDown {
+                            Task {
+                                self.pageNumber += 1
+                                await viewModel.getRepos(pageNumber: pageNumber)
+                            }
+                        }
+                    })
+            }
+            .navigationTitle("Repository List")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color.systemGroupedBackground)
         }
-        .navigationTitle("Repository List")
-        .navigationBarTitleDisplayMode(.inline)
-        .background(Color.systemGroupedBackground)
     }
 }
 
